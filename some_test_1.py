422d4f2c04e95d0dd28ed3567fcec7574a8796f9
Khnyrev <aleksey.khnyrev@dualbootpartners.com>
2023-06-06 22:53:36 +0400

    add new file some_test_2.py covered negative login case

---
+++ some_test_1.py
@@ -0,0 +1,40 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(
+    executable_path='/Users/alekseykhnyrev/PycharmProjects/pythonSeleniumProject/selenium_project/geckodriver')
+base_url = 'https://www.saucedemo.com'
+driver.get(base_url)
+login_standard_user = 'standard_user'
+password_all = "secret_sauce"
+
+driver.maximize_window()
+
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")  # XPAth
+user_name.send_keys(login_standard_user)
+print('Input Login')
+
+password = driver.find_element(By.XPATH, "//input[@id='password']")  # css selector
+password.send_keys(password_all)
+print('Input Password')
+
+button_login = driver.find_element(By.XPATH, '//input[@id="login-button"]')
+button_login.click()
+print('Click Login button')
+
+text_products = driver.find_element(By.XPATH, "//span[@class='title']")
+value_textproducts = text_products.text
+print(value_textproducts)
+assert value_textproducts == "Products"
+print('every thing alright'.upper())
+
+
+url = 'https://www.saucedemo.com/inventory.html'
+get_url = driver.current_url
+print(get_url)
+assert url == get_url
+print('GOOOOD URL')
+
+time.sleep(1)
+driver.close()
